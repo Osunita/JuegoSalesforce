5b0a65ad79517abad8bddfbae9944266bdae1a4e
Osunita <raulosunaabadias71@gmail.com>
2023-03-08 10:09:41 +0100

    preparadisimo

--- ExamenJuego/force-app/main/default/aura/tablero/tableroController.js
+++ ExamenJuego/force-app/main/default/aura/tablero/tableroController.js
@@ -20,25 +20,48 @@
                         component.find("2").set("v.clase", "limpia");
                         component.find("3").set("v.clase", "limpia");
                         component.find("4").set("v.clase", "limpia");
+                        component.find("5").set("v.clase", "limpia");
+                        component.find("6").set("v.clase", "limpia");
                         break;
                     case 2:
                         component.find("1").set("v.clase", "limpia");
                         component.find("2").set("v.clase", "color");
                         component.find("3").set("v.clase", "limpia");
                         component.find("4").set("v.clase", "limpia");
+                        component.find("5").set("v.clase", "limpia");
+                        component.find("6").set("v.clase", "limpia");
                         break;
                     case 3:
                         component.find("1").set("v.clase", "limpia");
                         component.find("2").set("v.clase", "limpia");
                         component.find("3").set("v.clase", "color");
                         component.find("4").set("v.clase", "limpia");
+                        component.find("5").set("v.clase", "limpia");
+                        component.find("6").set("v.clase", "limpia");
                         break;
                     case 4:
                         component.find("1").set("v.clase", "limpia");
                         component.find("2").set("v.clase", "limpia");
                         component.find("3").set("v.clase", "limpia");
                         component.find("4").set("v.clase", "color");
+                        component.find("5").set("v.clase", "limpia");
+                        component.find("6").set("v.clase", "limpia");
                         break;
+                    case 5:
+                        component.find("1").set("v.clase", "limpia");
+                        component.find("2").set("v.clase", "limpia");
+                        component.find("3").set("v.clase", "limpia");
+                        component.find("4").set("v.clase", "limpia");
+                        component.find("5").set("v.clase", "color");
+                        component.find("6").set("v.clase", "limpia");
+                        break;
+                    case 6:
+                        component.find("1").set("v.clase", "limpia");
+                        component.find("2").set("v.clase", "limpia");
+                        component.find("3").set("v.clase", "limpia");
+                        component.find("4").set("v.clase", "limpia");
+                        component.find("5").set("v.clase", "limpia");
+                        component.find("6").set("v.clase", "color");
                         }
             }
         });
